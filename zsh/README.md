47e74afcbdbabd812a25635a2f87f02e19086807
Erik Gahner Larsen <erikgahner@gmail.com>
2024-02-07 12:45:13 +0100

    add zsh files

---
+++ zsh/README.md
@@ -0,0 +1,17 @@
+# zsh configuration
+
+## 1. .zshrc
+
+Add `.zshrc` to the local version found here:
+
+```bash
+~/.zshrc
+```
+
+## 2. custom.zsh
+
+Add `custom.zsh` to the local version found here:
+
+```bash
+~/.oh-my-zsh/custom/custom.zsh
+```
---
+++ zsh/custom.zsh
@@ -0,0 +1,12 @@
+# aliases
+alias bc="brew cleanup"
+alias bi="brew info"
+alias bl="brew list"
+alias bu="brew update"
+alias bup="brew upgrade"
+alias gs="git status"
+alias e="exit"
+alias r="radian"
+
+# shortcuts
+omz=~/.oh-my-zsh/custom/custom.zsh
